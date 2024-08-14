07325a3ef7bc4785fd24e77c749c385ae0f1d9e2
JLDC047 <166645969+JLDC047@users.noreply.github.com>
2024-08-14 18:58:56 +0100

    Update Visualisation.py

--- Visualisation.py
+++ Visualisation.py
@@ -1,5 +1,6 @@
 import matplotlib.pyplot as plt
 import numpy as np
+import pandas as pd
 
 #create line plot
 x = [1,2,7,16,25]
@@ -19,3 +20,29 @@ plt.plot(x,y2,"bx-",label="y = x^3",linewidth=3)
 plt.title("y = x^2 and y = x^3")
 plt.legend()
 plt.show()
+
+#bar graph
+plt.bar([1,3,5,7],[6,5,2,9],color="b")
+plt.bar([2,4,6,8],[10,7,7,4],color="r")
+plt.show()
+
+plt.bar(["Apple","Pineapple","Crab apple"],[6,2,23])
+plt.show()
+
+#showing the number of male and female on titanic
+titanic = pd.read_csv("titanic.csv")
+sex = titanic["Sex"].value_counts()
+
+"""plt.bar("Male",sex["male"],color="b")
+plt.bar("Female",sex["female"],color="r")"""
+plt.bar(sex.index,sex.values)
+plt.show()
+
+#histogram
+ages = np.random.randint(0,80,50)
+bins = np.arange(0,81,10)
+
+plt.hist(ages,bins,rwidth=0.9,color="g",align="mid")
+#plt.yticks()
+plt.show()
+
