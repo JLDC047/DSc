5cd8a026a16c2e056ec57a59c22352d7027407cf
JLDC047 <166645969+JLDC047@users.noreply.github.com>
2024-09-04 18:58:19 +0100

    Update PreProccessingSteps.py

--- PreProccessingSteps.py
+++ PreProccessingSteps.py
@@ -5,7 +5,7 @@ import pandas as pd
 data = pd.read_csv("Data.csv")
 "print(data.info())"
 
-#split features and target
+#split features/indepedant variable and target/dependant variable
 X = data.iloc[:,:-1]
 y = data.iloc[:,-1]
 print(X)
@@ -16,6 +16,24 @@ print(X.isnull().sum())
 
 #replacing the missing the values
 from sklearn.impute import SimpleImputer
+imputer = SimpleImputer(missing_values=np.nan,strategy="mean")
+X.iloc[:,-2:] = imputer.fit_transform(X.iloc[:,-2:])
+print(X)
 
+#encoding non-numerical values
+#features
+X_encoded = pd.get_dummies(X,columns=["Country"],dtype=int,drop_first=True)
+print(X_encoded)
+#target
+from sklearn.preprocessing import LabelEncoder
+le = LabelEncoder()
+y_encoded = le.fit_transform(y)
+print(y_encoded)
 
-
+#split data into training and testing 
+from sklearn.model_selection import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X_encoded,y_encoded,test_size=0.3,random_state=6)
+print(X_train)
+print(X_test)
+print(y_train)
+print(y_test)
